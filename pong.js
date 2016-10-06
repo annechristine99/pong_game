eb54af2ddc70631ce288b921e4996e17ad900806
Annette Perez <annechristine99biz@gmail.com>
2016-10-06 17:38:35 -0400

    velocity for the first pong

--- pong.js
+++ pong.js
@@ -14,9 +14,32 @@ class Rect
       this.pos = new Vec;
       this.size = new Vec(w, h);
     }
+    get left()
+    {
+      return this.pos.x - this.size.x /2;
+    }
+    get right()
+    {
+      return this.pos.x + this.size.x /2;
+    }
+    get top()
+    {
+      return this.pos.y - this.size.y /2;
+    }
+    get bottom()
+    {
+      return this.pos.y + this.size.y /2;
+    }
 }
 
-//class Ball extends Rect
+class Ball extends Rect
+{
+    constructor()
+    {
+      super(10, 10);
+      this.vel = new Vec;
+    }
+}
 
 
 
@@ -25,8 +48,43 @@ class Rect
 const canvas = document.getElementById('pong');
 const context = canvas.getContext('2d');
 
-context.fillStyle = 'black';
-context.fillRect(0, 0, canvas.width, canvas.height);
+const ball = new Ball;
+//console.log(ball);
+ball.pos.x = 100;
+ball.pos.y = 50;
 
-context.fillStyle = 'orange';
-context.fillRect(0, 0, 10, 10);
+ball.vel.x = 100;
+ball.vel.y = 100;
+
+let lastTime;
+function callback(millis) {
+  if (lastTime) {
+    update((millis - lastTime) / 1000);
+  }
+  lastTime = millis;
+  requestAnimationFrame(callback);
+}
+
+
+
+function update(dt) {
+  ball.pos.x += ball.vel.x * dt;
+  ball.pos.y += ball.vel.y * dt;
+
+  if (ball.left < 0 || ball.right > canvas.width) {
+      ball.vel.x = -ball.vel.x;
+  }
+  if (ball.top < 0 || ball.bottom > canvas.height) {
+      ball.vel.y = -ball.vel.y;
+  }
+
+
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = 'orange';
+  context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
+
+}
+
+callback();
